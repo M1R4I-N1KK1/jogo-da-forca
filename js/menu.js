5dade644c7e8fae1d4392e9472788c5c69e21b19
M1R4I-N1KK1 <ygorf3535@gmail.com>
2022-02-10 20:34:18 -0300

    respossividade

--- js/menu.js
+++ js/menu.js
@@ -9,21 +9,30 @@ function clearTeclado(){
 	document.querySelectorAll("button").classList.add('clicado');
 };
 
+function restart(){
+	menu.style.display = 'none'
+	game.tentativas = 6;
+	game.letras_usada = [];
+	game.saida = [];
+	
+	clearTela();
+	
+	for (let i = palavra.palavra.length; i > 1; i--) {
+		document.querySelector("#local-palavra").innerHTML -= '<div class="letra" id="index'+[i]+'"></div>'
+	};
+	 
+	for (let i = 0; i < palavra.palavra.length; i++) {
+	  document.querySelector("#local-palavra").innerHTML += '<div class="letra" id="index'+[i]+'"></div>'
+	};
+};
+
 function start(){
 	menu.style.display = 'none'
 	game.tentativas = 6;
 	game.letras_usada = [];
 	game.saida = [];
-
-	clearTela()
 	
-	for (let i = 0; i < palavra.palavra.length; i++) {
-		document.querySelector("#local-palavra").innerHTML -= '<div class="letra" id="index'+[i]+'"></div>'
-	}
-
 	for (let i = 0; i < palavra.palavra.length; i++) {
 	  document.querySelector("#local-palavra").innerHTML += '<div class="letra" id="index'+[i]+'"></div>'
 	}
-};
-
-start()
+}
